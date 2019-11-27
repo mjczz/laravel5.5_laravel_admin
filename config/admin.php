bc81506d06142b78c8caf154312c7ed59ad7d2c7
mjczz <1004578796@qq.com>
2019-11-27 17:55:37 +0800

    安装一些larave_admin_extesion

--- config/admin.php
+++ config/admin.php
@@ -399,6 +399,12 @@ return [
         'china-distpicker' => [
             // 如果要关掉这个扩展，设置为false
             'enable' => true,
+        ],
+        'wxmenu' => [
+            'enable' => true,
+        ],
+        'composer-viewer' => [
+            'enable' => true,
         ]
     ],
 ];
